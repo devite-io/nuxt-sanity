f6891a5a19023fccd763e45384ac1525326f7233
Justus Geramb <admin@justix.dev>
2024-09-21 13:00:19 +0200

    fix: return type of helper function

--- src/runtime/utils/resolveImageAssetById.ts
+++ src/runtime/utils/resolveImageAssetById.ts
@@ -3,5 +3,5 @@ import type { ImageAsset } from '@sanity/types'
 import { IMAGE_ASSET_PROJECTION } from './projections'
 
 export const resolveImageAssetById = async (id: string): Promise<ImageAsset | undefined> => {
-  return await useSanityQuery<ImageAsset>(`*[_type == "sanity.imageAsset" && _id == $assetId][0] ${IMAGE_ASSET_PROJECTION}`, { assetId: id })
+  return (await useSanityQuery<ImageAsset>(`*[_type == "sanity.imageAsset" && _id == $assetId][0] ${IMAGE_ASSET_PROJECTION}`, { assetId: id })).data.value
 }
