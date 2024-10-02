fa1bb6a122f26a1304b1731126aa5bdb71eadb4c
Justus Geramb <admin@justix.dev>
2024-10-02 13:54:18 +0200

    feat: automatically resolve image references

--- src/runtime/components/SanityImageAsset.vue
+++ src/runtime/components/SanityImageAsset.vue
@@ -1,14 +1,14 @@
 <template>
   <NuxtImg
-    v-if="asset?._id"
+    v-if="imageAsset?._id"
     densities="x1 x2"
-    :src="asset._id"
-    :width="asset.metadata.dimensions.width"
-    :height="asset.metadata.dimensions.height"
-    :alt="asset.altText"
-    :placeholder="asset.metadata.lqip"
+    :src="imageAsset._id"
+    :width="imageAsset.metadata.dimensions.width"
+    :height="imageAsset.metadata.dimensions.height"
+    :alt="imageAsset.altText"
+    :placeholder="imageAsset.metadata.lqip"
     :loading="loading"
-    :format="asset.mimeType === 'image/svg+xml' ? undefined : 'webp'"
+    :format="imageAsset.mimeType === 'image/svg+xml' ? undefined : 'webp'"
     draggable="false"
   />
 </template>
@@ -16,5 +16,6 @@
 <script setup lang="ts">
 import type { ImageAsset } from '@sanity/types'
 
-const { loading = 'lazy' } = defineProps<{ asset: ImageAsset, loading?: 'lazy' | 'eager' }>()
+const { asset, loading = 'lazy' } = defineProps<{ asset?: ImageAsset, loading?: 'lazy' | 'eager' }>()
+const imageAsset = asset?._ref ? await resolveImageAssetById(asset._ref) : asset
 </script>
--- src/runtime/utils/resolveImageAssetById.ts
+++ src/runtime/utils/resolveImageAssetById.ts
@@ -1,7 +1,8 @@
 import { useSanityQuery } from '@nuxtjs/sanity/runtime/composables'
 import type { ImageAsset } from '@sanity/types'
 import { IMAGE_ASSET_PROJECTION } from './projections'
+import type { Ref } from '#imports'
 
-export const resolveImageAssetById = async (id: string): Promise<ImageAsset | undefined> => {
-  return (await useSanityQuery<ImageAsset>(`*[_type == "sanity.imageAsset" && _id == $assetId][0] ${IMAGE_ASSET_PROJECTION}`, { assetId: id })).data.value
+export const resolveImageAssetById = async (id: string): Promise<Ref<ImageAsset | undefined>> => {
+  return (await useSanityQuery<ImageAsset>(`*[_type == "sanity.imageAsset" && _id == $assetId][0] ${IMAGE_ASSET_PROJECTION}`, { assetId: id })).data
 }
