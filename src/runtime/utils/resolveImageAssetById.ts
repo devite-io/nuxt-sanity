5522e17006780d585ee4f8dfe77627e991def59f
Justus Geramb <admin@justix.dev>
2024-10-21 11:30:20 +0200

    fix: use visual editing composable + change return type of utility function

--- src/runtime/utils/resolveImageAssetById.ts
+++ src/runtime/utils/resolveImageAssetById.ts
@@ -1,8 +1,8 @@
-import { useSanityQuery } from '@nuxtjs/sanity/runtime/composables'
+import { useSanityQuery } from '@nuxtjs/sanity/runtime/composables/visual-editing'
 import type { ImageAsset } from '@sanity/types'
 import { IMAGE_ASSET_PROJECTION } from './projections'
 import type { Ref } from '#imports'
 
-export const resolveImageAssetById = async (id: string): Promise<Ref<ImageAsset | undefined>> => {
+export const resolveImageAssetById = async (id: string): Promise<Ref<ImageAsset | null>> => {
   return (await useSanityQuery<ImageAsset>(`*[_type == "sanity.imageAsset" && _id == $assetId][0] ${IMAGE_ASSET_PROJECTION}`, { assetId: id })).data
 }
