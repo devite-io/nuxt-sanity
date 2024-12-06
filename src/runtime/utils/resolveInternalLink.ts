f686cbee32d44d529d3ccfaf7c838ef7fa6460f1
Justus Geramb <admin@justix.dev>
2024-12-06 16:54:45 +0100

    fix: query parameter

--- src/runtime/utils/resolveInternalLink.ts
+++ src/runtime/utils/resolveInternalLink.ts
@@ -3,6 +3,6 @@ import type { ImageAsset, Reference } from '@sanity/types'
 import type { LinkInternal } from '@devite/nuxt-sanity'
 import type { Ref } from '#imports'
 
-export const resolveInternalLink = async (reference: Reference): Promise<Ref<LinkInternal | null>> => {
-  return (await useSanityQuery<ImageAsset>(`*[( _type == "home" || _type == "page") && _id == $documentId][0] { '_type': 'linkInternal', 'slug': coalesce(@.slug.current, '/') }`, { $documentId: reference._ref })).data
+export const resolveInternalLink = async (reference: Reference): Promise<Ref<LinkInternal | undefined>> => {
+  return (await useSanityQuery<ImageAsset>(`*[_id == $documentId][0] { '_type': 'linkInternal', 'slug': coalesce(slug.current, '/') }`, { documentId: reference._ref })).data
 }
