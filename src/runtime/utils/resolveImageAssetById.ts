84ca478ddd353b071f323cac44e5968b42f2613f
Justus Geramb <admin@justix.dev>
2024-12-06 09:30:02 +0100

    feat: change return type from null to undefined in function
    Updated the return type of `resolveImageAssetById` function from `null` to `undefined` to better align with TypeScript conventions.

--- src/runtime/utils/resolveImageAssetById.ts
+++ src/runtime/utils/resolveImageAssetById.ts
@@ -3,6 +3,6 @@ import type { ImageAsset } from '@sanity/types'
 import { IMAGE_ASSET_PROJECTION } from './projections'
 import type { Ref } from '#imports'
 
-export const resolveImageAssetById = async (id: string): Promise<Ref<ImageAsset | null>> => {
+export const resolveImageAssetById = async (id: string): Promise<Ref<ImageAsset | undefined>> => {
   return (await useSanityQuery<ImageAsset>(`*[_type == "sanity.imageAsset" && _id == $assetId][0] ${IMAGE_ASSET_PROJECTION}`, { assetId: id })).data
 }
