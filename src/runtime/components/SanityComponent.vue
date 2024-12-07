94ade62ff41a52dd0223acca0150a939e98a6546
Justus Geramb <admin@justix.dev>
2024-12-07 01:57:12 +0100

    fix: dynamic component slots + component resolve logic

--- src/runtime/components/SanityComponent.vue
+++ src/runtime/components/SanityComponent.vue
@@ -5,7 +5,15 @@
     ref="componentRef"
     v-bind="{ ...$props, ...$attrs }"
   >
-    <slot />
+    <template
+      v-for="(_, slotName) in $slots"
+      #[slotName]="slotProps"
+    >
+      <slot
+        :name="slotName"
+        v-bind="slotProps ?? {}"
+      />
+    </template>
   </component>
 </template>
 
@@ -14,24 +22,28 @@ import type { Component } from '@nuxt/schema'
 import { computed, ref, resolveComponent } from '#imports'
 import { SanityLinkExternal, SanityLinkInternal, SanityRichText } from '#components'
 
-const { data } = defineProps<{ data?: object }>()
+const { data } = defineProps<{ data?: { _type?: string } | Array<{ _type: string }> }>()
 
-const component = computed<Component>(() => {
-  const type = data?._type
+const component = computed<Component | null>(() => {
+  if (Array.isArray(data) && data.every((item) => item._type === 'block')) {
+    return SanityRichText
+  }
 
-  switch (type) {
-    case 'linkInternal':
-      return SanityLinkInternal
-    case 'linkExternal':
-      return SanityLinkExternal
-    default:
-      if (data?.constructor.name === 'Array' && data.every((item) => item._type === 'block'))
-        return SanityRichText
-      else if (type) {
+  if (data && typeof data === 'object' && '_type' in data) {
+    const type = data._type as string
+
+    switch (type) {
+      case 'linkInternal':
+        return SanityLinkInternal
+      case 'linkExternal':
+        return SanityLinkExternal
+      default: {
         const upperCamelCase = type.charAt(0).toUpperCase() + type.slice(1)
+        const resolvedComponent = resolveComponent('Sanity' + upperCamelCase)
 
-        return resolveComponent('Sanity' + upperCamelCase)
+        return resolvedComponent ? (resolvedComponent as Component) : null
       }
+    }
   }
 
   return null
