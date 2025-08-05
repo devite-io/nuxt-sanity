af870986cb1df13e17f17cd70eee532fc3eeef86
Justus Geramb <admin@justix.dev>
2025-08-05 12:15:57 +0200

    fix: prevent server side import of useState

--- src/runtime/imageProviders/sanity.ts
+++ src/runtime/imageProviders/sanity.ts
@@ -1,12 +1,11 @@
 import type { ImageCTX, ImageOptions, ResolvedImage } from '@nuxt/image'
-import { useSanityVisualEditingState } from '../composables/visual_editing_state'
 import { getImage as getSanityImage } from '#image/providers/sanity'
 
 export function getImage(src: string,
   { modifiers = {}, projectId, dataset, cacheEndpoint }: ImageOptions, context: ImageCTX): ResolvedImage {
   modifiers.format = modifiers.format === 'svg+xml' ? undefined : modifiers.format
 
-  if (cacheEndpoint && !(import.meta.client && useSanityVisualEditingState().enabled)) {
+  if (cacheEndpoint) {
     const params = new URLSearchParams()
     params.set('src', src)
 
