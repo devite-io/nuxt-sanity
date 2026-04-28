dba7d47a65d1c9d2c3a2403598934821572066be
Justus Geramb <admin@justix.dev>
2026-04-28 23:39:37 +0200

    fix: listen for property changes

--- src/runtime/components/link/SanityLinkInternal.vue
+++ src/runtime/components/link/SanityLinkInternal.vue
@@ -11,10 +11,14 @@
 <script setup lang="ts">
 import { resolveInternalLink } from '#imports'
 import type { LinkInternal } from '@devite/nuxt-sanity'
-import type { Ref } from 'vue'
+import type { Reference } from '@sanity/types'
+import { computed, type ComputedRef, type Ref } from 'vue'
 
-const { data } = defineProps<{ data: LinkInternal | { reference: Reference } }>()
-const resolvedLink: Ref<LinkInternal | null> | LinkInternal = 'reference' in data
-  ? await resolveInternalLink(data.reference)
-  : data as LinkInternal
+const { data } = defineProps<{
+  data: LinkInternal | { reference: Reference }
+}>()
+const resolvedLink: Ref<LinkInternal | null> | ComputedRef<LinkInternal>
+  = 'reference' in data
+    ? await resolveInternalLink(computed(() => data.reference))
+    : computed(() => data as LinkInternal)
 </script>
--- src/runtime/utils/resolveInternalLink.ts
+++ src/runtime/utils/resolveInternalLink.ts
@@ -1,11 +1,11 @@
-import type { Reference } from '@sanity/types'
-import type { LinkInternal } from '@devite/nuxt-sanity'
-import type { Ref } from 'vue'
 import { useSanityQuery } from '#imports'
+import type { LinkInternal } from '@devite/nuxt-sanity'
+import type { Reference } from '@sanity/types'
+import { computed, type MaybeRef, type Ref, toValue } from 'vue'
 
-export const resolveInternalLink = async (reference: Reference): Promise<Ref<LinkInternal | null>> => {
+export const resolveInternalLink = async (reference: MaybeRef<Reference>): Promise<Ref<LinkInternal | null>> => {
   return (await useSanityQuery<LinkInternal>(
     `*[_id == $documentId][0] { '_type': 'linkInternal', 'slug': '/' + coalesce(slug.current, ''), linkTitle, relationship }`,
-    { documentId: reference._ref },
+    { documentId: computed(() => toValue(reference)._ref) },
   )).data
 }
